26caf3058d0952b8648ae59119ca03246a7baed6
FernandoHeimrik <fernando.hnrq@gmail.com>
2020-03-26 01:28:37 -0300

    rotas, controllers, crud

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,21 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-    const body = request.body;
-    
-    console.log(body);
-    
-    return response.json({
-        evento: "Semana OminiStack 11.0",
-        aluno: "Fernando Henrique"
-    });
-});
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
