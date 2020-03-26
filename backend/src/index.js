c4b7e08a064b9a87df31b8061db5e1a5455e56e3
FernandoHeimrik <fernando.hnrq@gmail.com>
2020-03-25 23:42:10 -0300

    configurando banco de dados

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express(); // Variavel armazena aplicação;
 
 app.use(express.json());
+app.use(routes);
 
 app.listen(3333) // mando a aplicação a ouvir a porta 3333
 
-app.get('/', (request, response) => {
-    return response.json({
-        aluno: "Fernando Henrique"
-    });
-})
