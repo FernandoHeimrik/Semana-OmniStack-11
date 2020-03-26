75d664cfae30108b57e9b428a47428decff27812
FernandoHeimrik <fernando.hnrq@gmail.com>
2020-03-26 01:49:14 -0300

    cors
    * npm install cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const app = express(); // Variavel armazena aplicação;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
