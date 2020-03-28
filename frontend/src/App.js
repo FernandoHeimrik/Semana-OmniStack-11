06f3840f6b0804408981bbe4356e42567f145603
Fernando Henrique <fernando.hnrq@gmail.com>
2020-03-28 15:12:35 -0300

    rotas, tela de Register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import "./global.css";
 
-//import Header from './Header';
-import Logon from './pages/Logon';
+import Routes from './routes';
 
 function App() {
 
   return (
-    <Logon />
+    <Routes />
   );
-
-  // const [counter, setCounter] = useState(0);
-
-  // // Array [valor, funcaoDeAtualizacao]
-
-  // function increment() {
-  //   setCounter(counter + 1);
-  // }
-
-  // return (
-  //   // <Header title="Semana OmniStack" />
-  //   // <Header>
-  //   //   Semana OmniStack
-  //   // </Header>
-  //   <div>
-  //     <Header>Contador: {counter}</Header>
-  //     <button onClick={increment}>Incrementar</button>
-  //   </div>
-  // );
 }
 
 export default App;
