e895d0c695358faa504eaa8f85bf7f9aa5e3657c
Fernando Henrique <fernando.hnrq@gmail.com>
2020-03-29 20:05:21 -0300

    Mobile da aplicação

---
+++ Anotacoes.js
@@ -0,0 +1,24 @@
+/**
+*	rotas
+*/
+npm install react-router-dom
+
+/**
+*	client Http
+*/
+npm install axios
+
+/**
+*	javascript format
+*/
+Intl
+
+/**
+*	passa uma funcao
+*/
+onClick={() => handleDeleteIncident(incident.id)}
+
+/**
+*	passa o retorno de uma funcao
+*/
+onClick={handleDeleteIncident(incident.id)}
